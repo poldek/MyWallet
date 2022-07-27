63605910c6686d4a2de6739cba92d1937f17f0ab
Grzegorz Rękawek <grekawek@ganso.pl>
2022-07-27 20:39:24 +0200

    Add link gitHub project

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -1,5 +1,5 @@
 <div class="footer">
     <slot>
-        © 2022 || Grzegorz <a href="mailto:rekawek@gmail.com">"Szycha"</a> Rekawek Ver: 0.1
+        © 2022 || Grzegorz <a href="mailto:rekawek@gmail.com">"Szycha"</a> Rekawek Ver: 0.1 project: <a href="https://github.com/poldek/MyWallet" target="_blank">GitHub</a>
     </slot>
 </div>
