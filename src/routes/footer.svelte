c3a93311c1e0686601adf01c32603c236d2d5b21
Grzegorz Rękawek <grekawek@ganso.pl>
2022-07-29 02:47:18 +0200

    Custom footer

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -1,5 +1,5 @@
 <div class="footer">
     <slot>
-        © 2022 || Grzegorz <a href="mailto:rekawek@gmail.com">"Szycha"</a> Rekawek Ver: 0.1 project: <a href="https://github.com/poldek/MyWallet" target="_blank">GitHub</a>
+        © 2022  <a href="https://www.linkedin.com/in/grzegorz-r%C4%99kawek-pgmsoft-ltd/" target="_blank">Linkin</a> <a href="https://github.com/poldek/MyWallet" target="_blank">GitHub</a>
     </slot>
 </div>
